c76f3ab66cfce6625e6f88489dc3fed6e656b580
Aiden Romero <aidenromerogarcia@gmail.com>
2025-02-24 10:07:18 +0100

    Fixed code CalculateDNI.java

--- src/main/java/org/example/CalculateDNI.java
+++ src/main/java/org/example/CalculateDNI.java
@@ -1,17 +1,12 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class CalculateDNI {
+    private static final String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+
     public static char calculateIDLetter(int idNum) {
-        List<Character> letters = Arrays.asList('T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F',
-                'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S',
-                'Q', 'V', 'H', 'L', 'C', 'K', 'E');
         if (idNum < 0 || idNum >= 99999999) {
-            throw new IllegalArgumentException("Not valid ID " + idNum);
+            throw new IllegalArgumentException("Not valid ID: " + idNum);
         }
-        int rest = idNum % 23;
-        return letters.get(rest);
+        return LETTERS.charAt(idNum % 23);
     }
 }
